8c85d11c9c65678bf9d3a51410890e3b254badb6
manuel perez <manuelperez.0000@gmail.com>
2025-03-21 18:43:08 -0600

    seo listo

--- src/components/Galeria/modal.jsx
+++ src/components/Galeria/modal.jsx
@@ -2,7 +2,7 @@ export const Modal = ({ close, active, image, anterior, siguiente, id }) =>
     active &&
     <div className='bg-modal'>
         <div class='body-modal'>
-            {image ? <img class={'img-modal-ext'} src={image} alt={'remodelaciones en costa rica'} /> : <h1>Sin imagen que mostrar</h1>}
+            {image ? <img class={'img-modal-ext'} src={image} alt={'remodelaciones en costa rica'} /> : <p>Sin imagen que mostrar</p>}
         </div>
         <div class={'modal-buttons'}>
             <button onClick={() => anterior(id)} class={'modal-arrows'}> <i class={'bi bi-chevron-left'} /> </button>
